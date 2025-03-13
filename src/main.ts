c78df2947c53cd25256c97710d20ba5ea5b5056b
ihor_hozhenko <lugtrauma@gmail.com>
2025-03-13 20:58:22 +0000

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+  }
+  
+  console.log(add(1, 2));
+  
+  // function foo (num: number, str: string, bool: boolean, empty: null) {
+  //   // Some logic
+  // }
